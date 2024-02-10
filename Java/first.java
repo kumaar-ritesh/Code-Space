ee09ee5d3bb061f35738bfbb23d3d0c9119f40ed
kumaar-ritesh <159596419+kumaar-ritesh@users.noreply.github.com>
2024-02-10 01:35:43 +0000

    my coding environment for Java

---
+++ Java/first.java
@@ -0,0 +1,14 @@
+class first
+{
+    public static void main(String[] args) {
+        System.out.println("A normal pattern");
+        for(int i =0;i<5;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
